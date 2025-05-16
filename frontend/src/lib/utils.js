d4b69be1bf3d2cc091a8b203acb624977bc56624
Chiranjeevi <chiranjeevinanda23@gmail.com>
2025-05-16 21:04:26 +0530

    Project Completed

---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,7 @@
+export const formatMessageTime = (data) => {
+    return new Date(data).toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+    });
+};
